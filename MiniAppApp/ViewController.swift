0d0e0c93f3fe3ac341f8ca19fa3f8c26afe74f54
Aleksandr Moskovtsev <aleks.moskovtsev@gmail.com>
2024-09-07 16:33:07 +0300

    Initial Commit

---
+++ MiniAppApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniAppApp
+//
+//  Created by user on 07.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
